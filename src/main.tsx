825ba99557b51004cf31209d265557ba348a3579
Roman Egel <reg-ext@marketlogicsoftware.com>
2024-09-22 20:55:16 +0100

    add validation

--- src/main.tsx
+++ src/main.tsx
@@ -19,22 +19,22 @@ fetch('https://telegram-bot-131843337439.europe-west3.run.app/oauth/validate', {
   .then(data => {
     if (data.valid) {
       console.log('Validation successful');
-      // Proceed with the app initialization
+      WebApp.ready();
+
+      ReactDOM.createRoot(document.getElementById('root')!).render(
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>,
+      )
     } else {
       console.error('Validation failed');
-      // Handle the case when validation fails
+      WebApp.showAlert('Failed to validate. Please try again later.');
+      WebApp.close();
     }
   })
   .catch(error => {
     console.error('Error during validation:', error);
     // Handle any errors that occurred during the fetch
+    WebApp.showAlert('Failed to validate. Please try again later.');
+    WebApp.close();
   });
-
-
-WebApp.ready();
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
