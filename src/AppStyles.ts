328a836a596d4bdc72bddc4ffc78c87f8149bd46
Roman Egel <reg-ext@marketlogicsoftware.com>
2024-09-26 11:20:15 +0100

    change card style

--- src/AppStyles.ts
+++ src/AppStyles.ts
@@ -65,7 +65,7 @@ export const createAppStyles = (colors: ThemeColors) => css`
     }
   }
   .app-card {
-    background-color: ${colors.section_separator_color};
+    background-color: ${colors.section_bg_color};
     border: 1px solid ${colors.hint_color}33; // 33 for 20% opacity
   }
   .app-hint {
